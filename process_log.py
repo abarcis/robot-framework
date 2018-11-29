3f86f8d32fa353119b56a39c295c599fbdfc6a81
Gniewek, Agata <agata.gniewek@aau.at>
2018-11-29 15:48:58 +0100

    Merge branch 'master' of gitlab.aau.at:aau-nav/development/sync_and_swarm

--- process_log.py
+++ process_log.py
@@ -15,10 +15,9 @@ import itertools
 # 'orientation_y', 'orientation_z', 'orientation_w', 'angle_xy'])
 
 
-def process_directory(directory):
+def get_files_from_directory(directory):
     hostnames = [f for f in os.listdir(directory)
                  if os.path.isdir(os.path.join(directory, f))]
-
     result = {}
     for h in hostnames:
         log_file_path = os.path.join(directory, h, 'log')
@@ -28,22 +27,48 @@ def process_directory(directory):
             continue
         result[h] = [f for f in os.listdir(log_file_path)
                      if os.path.isfile(os.path.join(log_file_path, f))]
+    return result
+
+
+def interactively_process_directory(directory):
+    files = get_files_from_directory(directory)
     response = ".*"
     while response != "":
-        result = {
+        files = {
             h: sorted([r for r in fs
                        if re.search(response, r)])
-            for h, fs in result.items()
+            for h, fs in files.items()
         }
-        pprint(result)
+        pprint(files)
         print("Stats:")
-        pprint({h: len(fs) for h, fs in result.items()})
-        print("Type in a regexp to filter the results or press [enter].")
+        pprint({h: len(fs) for h, fs in files.items()})
+        print("Type in a regexp to filter the filess or press [enter].")
         response = input()
     process_files(
         sum([[os.path.join(directory, h, 'log', f) for f in fs]
-             for h, fs in result.items()], [])
+             for h, fs in files.items()], [])
     )
+
+
+def generate_preview(directory, output_directory):
+    files = get_files_from_directory(directory)
+    all_filenames = itertools.chain(*files.values())
+    all_dates = set(n[:19] for n in all_filenames)
+    dates_in_2018 = [n for n in all_dates
+                     if n.startswith("2018")]
+
+    for d in dates_in_2018:
+        filepaths = itertools.chain(*[
+            [
+                os.path.join(directory, h, 'log', f) for f in fs
+                if f.startswith(d)
+            ]
+            for h, fs in files.items()
+        ])
+        process_files(
+            filepaths,
+            os.path.join(output_directory, "{}.png".format(d))
+        )
 
 
 def parse_file(filepath):
@@ -75,46 +100,72 @@ def parse_file(filepath):
     return together
 
 
-def process_files(filepaths):
+def process_files(filepaths, output_file=None):
     datas = [parse_file(f) for f in filepaths]
+    datas = [d for d in datas
+             if d != {}]
 
-    assert all(set(datas[0].keys()) == set(d.keys()) or d == {} for d in datas)
+    if not all(set(datas[0].keys()) == set(d.keys()) for d in datas):
+        import ipdb
+        ipdb.set_trace()
     start = 0
     end = -1
 
+    if datas == []:
+        print("WARN: no data given")
+        return
+
+    def merge_data(datas):
+        if True:  # sort by date
+            all_sorted = sorted(itertools.chain(*datas))
+            for e in all_sorted:
+                yield e[1]
+
+        else:
+            da = list(datas)
+            longest_length = max([len(a) for a in da])
+            for i in range(longest_length):
+                for d in da:
+                    idx = len(d) - longest_length + i
+                    if idx >= 0:
+                        yield d[idx][1]
     data = {
-        key: list(itertools.chain(
-            *zip(*[d.get(key, [])[start:end] for d in datas])
-        ))
+        key: list(merge_data(
+            tuple(zip(d.get('timestamp', []), d.get(key, []))) for d in datas)
+        )
         for key in datas[0].keys()
     }
-    start = 0
-
-    val = 1
-
     phases = data['phase']
-
-    colors_rgb = [
-        colorsys.hsv_to_rgb(
-            phase/(2*np.pi),
-            i/len(phases),
-            val
-        )
-        for i, phase in enumerate(phases)
-    ]
-    # colors_rgba = [c + (i/len(colors_rgb), )
-    #                for i, c in enumerate(colors_rgb)]
-    colors = colors_rgb
 
     x = data['pos_x']
     y = data['pos_y']
 
     while True:
-        plt.scatter(x[start:end], y[start:end], c=colors[start:end])
-        plt.show()
         plt.clf()
         plt.cla()
         plt.close()
+        plt.figure(num=None, figsize=(12, 12), dpi=100, facecolor='w',
+                   edgecolor='k')
+        plt.axis('off')
+        ax = plt.gca()
+        ax.set_xlim(-1, 1)
+        ax.set_ylim(-0.9, 1.1)
+
+        hues = [phase/(2*np.pi) for phase in phases[start:end]]
+        colors = [
+            colorsys.hsv_to_rgb(
+                hue,
+                i/len(hues),
+                1
+            )
+            for i, hue in enumerate(hues)
+        ]
+        plt.scatter(x[start:end], y[start:end], c=colors)
+        if output_file is not None:
+            plt.savefig(output_file, bbox_inches='tight')
+            return
+        plt.savefig("output.png")
+        plt.show()
         limits = input("Change limits (format: start:stop)")
         start, end = map(int, limits.split(":"))
 
@@ -124,6 +175,9 @@ if __name__ == '__main__':
         print("First argument should be a filepath")
         sys.exit(1)
     if os.path.isdir(sys.argv[1]):
-        process_directory(sys.argv[1])
+        if len(sys.argv) > 2:
+            generate_preview(sys.argv[1], sys.argv[2])
+        else:
+            interactively_process_directory(sys.argv[1])
     else:
         process_files(sys.argv[1:])
