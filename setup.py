6fed4ed95dc9bb12420cd836a643861e8b82a065
Gniewek, Agata <agata.gniewek@aau.at>
2018-11-29 16:57:51 +0100

    setup.py created

---
+++ setup.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+    name="robot_framework",
+    version="0.0.0",
+    description="A framework for simulation and basic visualization of robots",
+    packages=setuptools.find_packages(),
+)
