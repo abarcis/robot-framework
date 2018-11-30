504eaade673a6b12dca8e4fc22183c410519b80e
Gniewek, Agata <agata.gniewek@aau.at>
2018-11-30 11:01:29 +0100

    ros2 package

--- setup.py
+++ setup.py
@@ -1,10 +1,37 @@
 #! /usr/bin/env python
 
-import setuptools
+from setuptools import setup
+from setuptools import find_packages
 
-setuptools.setup(
-    name="robot_framework",
-    version="0.0.0",
+package_name = 'robot_framework'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=[
+        'setuptools',
+        'numpy',
+        'pyquaternion',
+    ],
+    zip_safe=True,
+    author='Agata Gniewek',
+    author_email='agata.gniewek@aau.at',
+    maintainer='Agata Gniewek',
+    maintainer_email='agata.gniewek@aau.at',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
     description="A framework for simulation and basic visualization of robots",
-    packages=setuptools.find_packages(),
+    license='TODO',
+    tests_require=['pytest'],
 )
