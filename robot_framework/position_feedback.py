354c6ba9c125d51cd7a95f2e8308ba6e1c9bedec
Gniewek, Agata <agata.gniewek@aau.at>
2019-11-06 12:08:38 +0100

    sync and swarm fixes

--- robot_framework/position_feedback.py
+++ robot_framework/position_feedback.py
@@ -2,5 +2,10 @@
 
 
 class PositionFeedback:
-    def get_new_position(self, state, velocity, time_delta):
-        return state.position + velocity * time_delta
+    def __init__(self, states, time_delta):
+        self.states = states
+        self.time_delta = time_delta
+
+    def get_new_position(self, ident):
+        state = self.states[ident]
+        return state.position + state.velocity * self.time_delta
