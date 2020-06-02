c31913a40af2a370c3e8dec57768b6267bd3afa4
Gniewek, Agata <agata.gniewek@aau.at>
2020-06-02 11:49:42 +0200

    convergence time experiment and plots

---
+++ robot_framework/main_convergence_time_experiment.py
@@ -0,0 +1,178 @@
+#! /usr/bin/env python
+
+import time
+
+from controller.synchronized_offline_controller import (
+    SynchronizedOfflineController
+)
+from controller.offline_controller import (
+    OfflineController
+)
+from logic.discrete_sync_and_swarm_logic import DiscreteLogic
+from logic.sync_and_swarm_logic import SyncAndSwarmLogic
+from position_feedback import PositionFeedback
+from knowledge.shared_knowledge import SharedKnowledge
+from communication.offline_communication import OfflineCommunication
+from system_state import SystemState
+from visualization.live_visualization import LiveVisualization
+from visualization.order_params_visualization import OrderParamsVisualization
+from logger.state_log import StateLog
+
+from utils.get_properties import estimate_radius
+from utils.pattern_formed import (
+    is_discrete_pattern_formed,
+    is_original_pattern_formed
+)
+import datetime
+import os
+import numpy as np
+
+import keyboard
+
+DEFAULT_IDENT = "a{}"
+SIM_NUMBER = 10
+
+
+def gen_pattern_formed_callback(vis, dirname, model='discrete'):
+    def pattern_formed_callback(t, states, params):
+        if model == 'discrete':
+            is_pattern_formed = is_discrete_pattern_formed
+            time_step = params['small_phase_steps'] * params['time_delta']
+        if model == 'original':
+            is_pattern_formed = is_original_pattern_formed
+            time_step = params['time_delta']
+        if is_pattern_formed(
+            states, params
+        ):
+            filename = (
+                f"{model}:"
+                f"dt={time_step}:"
+                f"t={t}:"
+                f"scale="
+                f"({params['attraction_factor']},{params['repulsion_factor']}):"
+                f".pdf"
+            )
+            vis.fig.savefig(f'{dirname}/{filename}')
+            print(t, "!!!!!FORMED!!!!")
+            return True
+        if t > 10000:
+            print(f"{model}:{time_step} NOT FORMED")
+            return True
+        return False
+    return pattern_formed_callback
+
+
+def main():
+    dirname = f'results/{datetime.datetime.now()}'
+    os.mkdir(dirname)
+    with keyboard.KeyPoller() as key_poller:
+        agents_num = 12
+        ids = [
+            DEFAULT_IDENT.format(i) for i in range(agents_num)
+        ]
+        time_deltas = np.arange(0.01, 0.5, 0.01)
+        params_presets = [
+            {'J': 0.1, 'K': 1, 'M': 1, 'name': "STATIC SYNC"},
+        ]
+        scale_factors = [(0.5, 2)]  # [(1, 1), (0.5, 2), (0.5, 1), (0.5, 4)]
+
+        small_phase_steps = 10
+        initial_params = {
+            'phase_levels_number': 18,
+            'agent_radius': 0.1,
+            'min_distance': 0.1,
+            'small_phase_steps': small_phase_steps,
+            'attraction_factor': 0.5,
+            'repulsion_factor': 2
+        }
+        initial_params.update(params_presets[0])
+        visualizations = [
+            OrderParamsVisualization(params=initial_params),
+            LiveVisualization(
+                agent_radius=initial_params['agent_radius']
+            ),
+        ]
+        discrete_pattern_formed_callback = gen_pattern_formed_callback(
+            visualizations[-1],
+            dirname,
+            'discrete'
+        )
+        original_pattern_formed_callback = gen_pattern_formed_callback(
+            visualizations[-1],
+            dirname,
+            'original'
+        )
+
+        # discrete modelA
+        print("DISCRETE")
+        for time_delta in time_deltas:
+            print(time_delta)
+            for attraction_factor, repulsion_factor in scale_factors:
+                for i in range(SIM_NUMBER):
+                    #for i in range(10):
+                    initial_params.update({
+                        'time_delta': time_delta,
+                        'attraction_factor': attraction_factor,
+                        'repulsion_factor': repulsion_factor,
+                    })
+
+                    logic = DiscreteLogic(initial_params)
+                    knowledge = SharedKnowledge(ids)
+                    system_state = SystemState(ids, knowledge, params=initial_params)
+                    position_feedback = PositionFeedback(system_state.states, time_delta)
+                    communication = OfflineCommunication(system_state)
+                    logger = StateLog(initial_params)
+                    controller = SynchronizedOfflineController(
+                        agents_num,
+                        logic=logic,
+                        position_feedback=position_feedback,
+                        communication=communication,
+                        system_state=system_state,
+                        visualizations=visualizations,
+                        logger=logger,
+                        # sleep_fcn=time.sleep,
+                        key_poller=key_poller,
+                        teleop_on=True,
+                        params_list=params_presets,
+                        mission_end_callback=discrete_pattern_formed_callback,
+                        time_delta=time_delta,
+                        small_phase_steps=small_phase_steps,
+                    )
+
+                    controller.run()
+
+        print("ORIGINAL")
+        time_delta = 0.1
+        for attraction_factor, repulsion_factor in scale_factors:
+            for i in range(SIM_NUMBER):
+                initial_params.update({
+                    'time_delta': time_delta,
+                    'attraction_factor': attraction_factor,
+                    'repulsion_factor': repulsion_factor,
+                })
+
+                logic = SyncAndSwarmLogic(initial_params)
+                knowledge = SharedKnowledge(ids)
+                system_state = SystemState(ids, knowledge, params=initial_params)
+                position_feedback = PositionFeedback(system_state.states, time_delta)
+                communication = OfflineCommunication(system_state)
+                logger = StateLog(initial_params)
+                controller = OfflineController(
+                    agents_num,
+                    logic=logic,
+                    position_feedback=position_feedback,
+                    communication=communication,
+                    system_state=system_state,
+                    visualizations=visualizations,
+                    logger=logger,
+                    # sleep_fcn=time.sleep,
+                    params_list=params_presets,
+                    mission_end_callback=original_pattern_formed_callback,
+                    time_delta=time_delta,
+                    small_phase_steps=small_phase_steps,
+                )
+
+                controller.run()
+
+if __name__ == "__main__":
+    main()
---
+++ robot_framework/plot_results.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python
+
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def parse_data(filenames):
+    data = {'original': {}, 'discrete': {}}
+    for f in filenames:
+        d = f.split(':')
+        model = d[0]
+        dt = d[1]
+        t = float(d[2].split('=')[1])
+        scale = d[3]
+        if scale not in data[model]:
+            data[model][scale] = {}
+        if dt not in data[model][scale]:
+            data[model][scale][dt] = []
+        data[model][scale][dt].append(t)
+    return data
+
+
+def plot_convergence_time(data):
+    f = plt.figure()
+    for scale, times in data['discrete'].items():
+        xs = []
+        ys = []
+        for k, t in times.items():
+            dt = float(k.split('=')[1])
+            xs.append(dt)
+            ys.append(sum(t)/len(t))
+
+        s = plt.scatter(xs, ys, label='swarmalatorbot')
+
+    for scale, times in data['original'].items():
+        for dt, val in times.items():
+            xs = np.arange(0, 5, 0.1)
+            avg_t = sum(val)/len(val)
+            ys = [avg_t for x in xs]
+            plt.plot(xs, ys, '--', color='grey', label='baseline')
+    plt.legend()
+    plt.xlabel('time step $\Delta T\, [\mathrm{s}]$')
+    plt.ylabel('convergence time $[\mathrm{s}]$')
+    plt.show()
+
+
+def main():
+    results_dir = 'results/'
+    all_result_dirs = sorted(os.listdir(results_dir))
+    results_filenames = os.listdir(f'{results_dir}{all_result_dirs[-1]}')
+    data = parse_data(results_filenames)
+    plot_convergence_time(data)
+
+
+if __name__ == "__main__":
+    main()
